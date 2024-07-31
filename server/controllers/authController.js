a974009cb0cf9425422270b0549062ad6bafd7a8
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-07-31 16:55:23 -0700

    Update login functionality to include token generation; Update logout functionality to store tokens in cookies

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken') // To generate JWT tokens
 const bcrypt = require('bcrypt') // To hash passwords
 
 
-
+// Register a new user
 async function register(req, res) {
 	console.log('Inside register controller');
 	const { username, email, first_name, last_name, password, password_confirm } = req.body // Validation of these fields will be done by Mongoose
@@ -19,9 +19,9 @@ async function register(req, res) {
 		return res.status(422).json({ 'message': 'Passwords do not match' });
 	}
 
-	const userExists = await User.exists({ email }).exec() // Check if user already exists in database
+	const userExists = await User.exists({ email }); // Check if user already exists in database
 
-	// If yes, return error
+	// If user exists, return error
 	if (userExists) {
 		return res.status(409).json({ 'message': 'User already exists' });
 	}
@@ -30,7 +30,8 @@ async function register(req, res) {
 	try {
 		hashedPassword = await bcrypt.hash(password, 10)
 
-		await User.create({ email, username, password: hashedPassword, first_name, last_name }); // Create user
+		// Create a new user
+		await User.create({ email, username, password: hashedPassword, first_name, last_name });
 
 		return res.sendStatus(201).json({ 'message': 'User created successfully' }); // Send response 201 (Created status)
 	} catch (error) {
@@ -39,36 +40,87 @@ async function register(req, res) {
 	}
 }
 
+// User login
 async function login(req, res) {
 	console.log('Inside login controller');
-	const { email, password } = req.body
+	const { email, password } = req.body // Extract email and password from request body
 
 	// Check if all required fields are present
 	if (!email || !password) {
 		return res.status(422).json({ 'message': 'Invalid fields' });
 	}
 
-	const user = await User.findOne({ email }) // Find the user by email
+	// Find the user by email
+	const user = await User.findOne({ email }).exec();
 
-	// Check if user is in database}
+	// Check if user is in database
 	if (!user) return res.sendStatus(401).json({ 'message': 'No users with this email exist' })
 
 	// Check if passwords match using bcrypt
-	const match = await bcrypt.match(password, user.password)
+	const match = await bcrypt.compare(password, user.password)
 	if (!match) return res.status(401).json({ 'message': 'Email or password is incorrect' })
 
+	// Generate access token
+	const accessToken = jwt.sign(
+		{
+			id: user.username
+		},
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: '1800s' // Expires in 30 min
+		}
+	)
+
+	// Generate refresh token
+	const refreshToken = jwt.sign(
+		{
+			username: user.username
+		},
+		process.env.REFRESH_TOKEN_SECRET,
+		{
+			expiresIn: '1d' // Expires in 1 day
+		}
+	)
+
+	// Save refresh token with the user
+	user.refresh_token = refreshToken;
+	await user.save();
+
+	// Set the refresh token as an HTTP-only cookie
+	res.cookie('refresh_token', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 1000, sameSite: 'None', secure: true }) // Refresh token will expire after 1 day
+	res.json({ access_token: accessToken })
 }
 
+// User logout
 async function logout(req, res) {
 	console.log('Inside logout controller');
-	res.sendStatus(200);
+	const cookies = req.cookies; // Get cookie
+	if (!cookies.refresh_token) return res.sendStatus(204); // If there is no cookie, send a No Content response
+
+	const refreshToken = cookies.refresh_token; // Get refresh token
+	const user = await User.findOne({ refresh_token: refreshToken }).exec(); // Access user from database through refresh token
+
+	// If the user does not exist in database... clear the cookie
+	if (!user) {
+		res.clearCookie('refresh token', { httpOnly: true, sameSite: 'None', secure: true }); // Note: sameSite means we will be able to use our cookies between cross-origin requests (e.g. we will be able to use cookies between the client and our server).
+		return res.sendStatus(204);
+	}
+
+	// If the user exists in database...
+	user.refresh_token = null; // Nullify refresh token
+	await user.save(); // Save the user
+
+	res.clearCookie('refresh_token', { httpOnly: true, sameSite: 'None', secure: true });
+	res.sendStatus(204);
 }
 
+// Refresh token
 async function refresh(req, res) {
 	console.log('Inside refresh controller');
 	res.sendStatus(200);
 }
 
+// Get user data
 async function user(req, res) {
 	console.log('Inside user controller');
 	res.sendStatus(200);
