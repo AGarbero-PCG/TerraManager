3388da510546447c087402a2bb551bc19e4fb44b
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-08-02 09:49:22 -0700

    Created authStore from pinia in auth.ts for handling states; started useApi.js for request and response handling

---
+++ client/src/composables/useApi.js
@@ -0,0 +1,1 @@
+// Used for request and response handling
--- client/src/stores/auth.ts
+++ client/src/stores/auth.ts
@@ -1,7 +1,56 @@
 // This file is for fetching and storing authenticated users
 import { defineStore } from "pinia"
 
+// This user model represents our state user
+export interface User {
+	id: number,
+	username: string,
+	email: string,
+	first_name: string,
+	last_name: string,
+}
+
+export interface State {
+	user: User
+}
+
 
 export const useAuthStore = defineStore('auth', {
+	// Used to store Data
+	state: (): State => {
+		return {
+			// Using this interface as a model for user state
+			user: {} as User
+		}
+	},
 
+	// Used to format state data
+	getters: {
+		user: (state: State) => state.user, // For returning user data
+		isAuthenticated: (state: State) => state.user?.id ? true : false // Return a boolean indicating whether user is authenticated or not
+	},
+
+	// Used to modify data inside the state
+	actions: {
+		// Asynchronous because we will use backend API requests
+		async login() {
+
+		},
+
+		async register() {
+
+		},
+
+		async getUser() {
+
+		},
+
+		async logout() {
+
+		},
+
+		async refresh() {
+
+		}
+	}
 })
