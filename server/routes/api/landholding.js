026e83c860f68d883244113c0161c204e953738c
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-08-06 18:18:48 -0700

    Created routes for Owner and LandHolding models

---
+++ server/routes/api/landholding.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const landHoldingController = require('../../controllers/landHoldingController');
+
+router.get('/', landHoldingController.getLandHoldings);
+router.get('/:id', landHoldingController.getLandHoldingById);
+router.post('/', landHoldingController.createLandHolding);
+router.put('/:id', landHoldingController.updateLandHolding);
+router.delete('/:id', landHoldingController.deleteLandHolding);
+
+module.exports = router;
