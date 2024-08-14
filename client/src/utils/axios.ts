96034961c72647f1a6765d7602d6905f5f9384ac
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-08-14 13:48:30 -0700

    Updated axios.ts to test baseURL issue

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_API_URI, // API endpoint for the backend
+	baseURL: 'https://terra-manager.onrender.com', // API endpoint for the backend: import.meta.env.VITE_API_URI
 	withCredentials: true, // Used to send HTTP-only cookies to the backend
 	headers: {
 		'Content-Type': 'application/json' // Necessary to be able to work with JSON data
