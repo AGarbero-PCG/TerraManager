59d6c4e98ab1784f6f85ab189394812d3075c7b0
AGarbero-PCG <agarbero@phxcapitalgroup.com>
2024-10-01 13:22:56 -0700

    Updated node_modules and began converting hosting to local for development

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: 'https://terra-manager.onrender.com', // API endpoint for the backend: import.meta.env.VITE_API_URI
+	baseURL: 'http://localhost:3500', // API endpoint for the backend: import.meta.env.VITE_API_URI
 	withCredentials: true, // Used to send HTTP-only cookies to the backend
 	headers: {
 		'Content-Type': 'application/json' // Necessary to be able to work with JSON data
@@ -10,7 +10,7 @@ export const axiosInstance = axios.create({
 });
 
 export const axiosPrivateInstance = axios.create({
-	baseURL: import.meta.env.VITE_API_URI,
+	baseURL: import.meta.env.VITE_API_URI || 'http://localhost:3500', // Fallback to local API endpoint if environment variable is not set
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json'
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
+// client/vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
