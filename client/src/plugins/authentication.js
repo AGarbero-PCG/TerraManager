a795636493f59a435f80ba7ceee98c06dc26be76
AGarbero-PCG <agarbero@phxcapitalgroup.com>
2024-10-08 09:35:39 -0700

    Update authentication.js to properly define methods

--- client/src/plugins/authentication.js
+++ client/src/plugins/authentication.js
@@ -9,44 +9,42 @@ const app = new Realm.App({ id: import.meta.env.VITE_REALM_APP_ID });
 console.log("Realm App ID:", import.meta.env.VITE_REALM_APP_ID);
 
 const authentication = {
-  install(app)  {
-    
-    // Register method
-    app.config.globalProperties.$register = async (email, password) => {
-      try {
-        await app.emailPasswordAuth.registerUser(email, password); // Register the user in MongoDB Realm
-        console.log("User registered successfully");
-        return response.data; // MongoDB Realm returns the user object
-      } catch (error){
-        console.error("Error registering user: " + error);
-        throw error;
-      }
-    };
+  
+  // Register method
+  async register (email, password) {
+    try {
+      await app.emailPasswordAuth.registerUser(email, password); // Register the user in MongoDB Realm
+      console.log("User registered successfully");
+      return response.data; // MongoDB Realm returns the user object
+    } catch (error){
+      console.error("Error registering user: " + error);
+      throw error;
+    }
+  },
 
-    // Login method
-    app.config.globalProperties.$login = async (email, password) => {
-      try {
-        const credentials = Realm.Credentials.emailPassword(email, password);
-        const user = await app.logIn(credentials); // Log the user in
-        return user; // MongoDB Realm returns the user object
-      } catch (error){
-        console.error("Error logging in: " + error);
-        throw error;
-      }
-    };
+  // Login method
+  async login(email, password) {
+    try {
+      const credentials = Realm.Credentials.emailPassword(email, password);
+      const user = await app.logIn(credentials); // Log the user in
+      return user; // MongoDB Realm returns the user object
+    } catch (error){
+      console.error("Error logging in: " + error);
+      throw error;
+    }
+  },
 
-    // Logout method
-    app.config.globalProperties.$logout = async () => {
-      try {
-        const user = app.currentUser;
-        if (user) {
-          await user.logOut(); // Log out the current user
-        }
-      } catch (error) {
-        console.error('Error logging out: ', error);
-        throw error;
+  // Logout method
+  async logout() {
+    try {
+      const user = app.currentUser;
+      if (user) {
+        await user.logOut(); // Log out the current user
       }
-    };
+    } catch (error) {
+      console.error('Error logging out: ', error);
+      throw error;
+    }
   }
 };
 
